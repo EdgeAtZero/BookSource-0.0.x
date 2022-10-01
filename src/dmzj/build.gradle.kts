0ac8a21980b003ec8c4acb006bb7aea7419ba60f
EdgeAtZero <edgeatzero@gmail.com>
2022-10-02 01:56:11 +0800

    push progress

--- src/dmzj/build.gradle.kts
+++ src/dmzj/build.gradle.kts
@@ -5,20 +5,25 @@ plugins {
     kotlin("plugin.serialization")
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation(project(":BookSource-api"))
-    implementation("io.ktor:ktor-client-core:2.0.3")
-    implementation("io.ktor:ktor-client-logging:2.0.3")
-    implementation("org.jsoup:jsoup:1.15.2")
+    implementation("io.ktor:ktor-client-core:2.1.2")
+    implementation("io.ktor:ktor-client-logging:2.1.2")
+    implementation("org.jsoup:jsoup:1.15.3")
     implementation("it.skrape:skrapeit-html-parser:1.2.2")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-properties:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-properties:1.4.0")
+
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-client-okhttp:2.0.3")
+    testImplementation("io.ktor:ktor-client-okhttp:2.1.2")
 }
 
 tasks {
